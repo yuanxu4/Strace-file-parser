ed048c3b56266f300b902a59bfb5366142d9fbc3
yuanxu <yuanxu4@illinois.edu>
2022-05-25 18:48:15 -0500

    can trace a simple syscall flow

--- strace_parser.py
+++ strace_parser.py
@@ -1,7 +1,9 @@
 import re
 import os
+from re import compile as recompile
+import turtle
 
-class file:
+class file_t:
     filepath = ""
     is_dir = -1
     create_time = ""
@@ -9,15 +11,7 @@ class file:
     fd_cur = -1
     size = -1
     off = -1
-    IOflow = []
-
-    def __init__(self, f, c, d, fd, s, o):
-        self.filepath = f
-        self.create_time = c
-        self.delete_time = d
-        self.fd_cur = fd
-        self.size = s
-        self.off = o
+    OPflow = []
 
     def out_file(self):
         print(f"This file is {self.filepath}")
@@ -40,41 +34,156 @@ class syscall:
         print(f"This sysop is {self.op}")
         print(f"This args is {self.args}")
         print(f"This retval is {self.retval}")
+    
+    def do_syscall(self):
+        if self.op == "openat":
+            #when open a dir
+            if "O_DIRECTORY" in self.args[-1]:
+                for file in files:
+                    if file.filepath == self.args[1]:
+                        file.is_dir = 1
+                        file.fd_cur = self.retval
+                        return 0
+                    new_file = file_t()
+                    new_file.filepath = self.args[1]
+                    new_file.fd_cur = self.retval
+                    new_file.is_dir = 1
+                    new_file.OPflow.append(self)
+                    files.append(new_file)
+                    return 0
+
+            for file in files:
+                #open a exsit file
+                if file.filepath == self.args[1]:
+                    file.fd_cur = self.retval
+                    file.OPflow.append(self)
+                    return 0
+            if "O_CREAT" in self.args[-1]:
+                #create a new file
+                new_file = file_t()
+                new_file.filepath = self.args[1]
+                new_file.fd_cur = self.retval
+                new_file.create_time = self.time
+                new_file.size = 0
+                new_file.off = 0
+                new_file.OPflow.append(self)
+                files.append(new_file)
+            else:
+                self.out_syscall()
+                print("cant open a file that not exist")
+                exit(0)
+
+        elif self.op == "close":
+            for file in files:
+                if file.fd_cur == int(self.args[0]):
+                    file.fd_cur = -1
+                    file.OPflow.append(self)
+                    return 0
+            self.out_syscall()
+            print("cant close a file that not open")
+            exit(0)
+
+        elif self.op == "write":
+            if int(self.args[-1]) != self.retval:
+                self.out_syscall()
+                print("write request size and reture size is not match")
+                exit(0)
+            for file in files:
+                if file.fd_cur == int(self.args[0]):
+                    if file.off == file.size:
+                        file.off += self.retval
+                        file.size += self.retval
+                    elif file.off != file.size:
+                        file.off += self.retval
+                        if file.off > file.size:
+                            file.size = file.off
+                    file.OPflow.append(self)
+                    return 0
+            self.out_syscall()
+            print("cant write into a file that not open")
+            exit(0)
         
-    
+        elif self.op == "lseek":
+            for file in files:
+                if file.fd_cur == int(self.args[0]):
+                    file.off = self.retval
+                    file.OPflow.append(self)
+                    return 0
+            self.out_syscall()
+            print("cant lseek change a file that not open")
+            exit(0)
+
+        elif self.op == "unlink":
+            for file in files:
+                if file.filepath == self.args[0]:
+                    file.fd_cur = -1
+                    file.delete_time = self.time
+                    file.OPflow.append(self)
+                    files.remove(file)
+                    deleted_files.append(file)
+                    return 0
+            self.out_syscall()
+            print("cant unlink a file that not exist")
+            exit(0)
+
+        # elif self.op == "fsync" | self.op == "fdatasync" | self.op == "pread64" | self.op == "fcntl" | self.op == "newfstatat" | self.op == "fstatfs":
+        #     for file in files:
+        #         if file.fd_cur == int(self.args[0]):
+        #             file.OPflow.append(self)
+        #             return 0
+        #     self.out_syscall()
+        #     print("cant fsync a file that not exist")
+        #     exit(0)
+        elif self.op == "fallocate":
+            for file in files:
+                if file.fd_cur == int(self.args[0]):
+                    if int(self.args[3]) > file.size:
+                        file.size = int(self.args[3])
+                    file.OPflow.append(self)
+                    return 0
+            self.out_syscall()
+            print("cant fallocate a file that not open")
+            exit(0)
+        else:
+            print("not support")
+            exit(0)
 
 
+def parse_strace(line):
+        START_RE = '(\d+\:\d+\:\d+\.\d+)[ ]+'    # match timestamp
+        CALL_RE = '(\w+)\((.*)\)'    # match syscall name & parameters
+        END_RE = '[ ]+= (?:(\d+)|(-\d+) (.*))'  # match return value
+        newsys = syscall()
+        RE = recompile(START_RE + CALL_RE + END_RE)
+        match = RE.match(line)
+        if not match:
+            print(line[:], "Line matching failed:")
+            exit(0)
+        groups = match.groups()
+        #print(groups)
+        newsys.time = groups[0]
+        newsys.op = groups[1]
+        newsys.args = groups[2].replace(" ", "").replace("\"", "").split(",")
+        newsys.retval = groups
+        if groups[3] is not None:
+            newsys.retval = int(groups[3])
+        #newsys.out_syscall()
+        return newsys
 
-    
+def show_files(files):
+    for file in files:
+        file.out_file()
+    return 0
 
-
-def parse_command(line):
-    # Find timestamp, format HH:MM:SS, and remove from line
-    new_line = syscall()
-    timestamp = re.findall('\d{2}:\d{2}:\d{2}.\d{6}',line)[0]
-    new_line.time = timestamp
-    #print(f"timestamp: {timestamp} \n")
-    line = line.replace(timestamp,'').strip()
-    if "()" in line:
-        command,rest = line.replace('()','')
-    command = re.findall('.+[(].+[)]',line)[0].strip()
-    rest = line.replace(command,'')
-    command,features = command.split('(',1)
-    features = [x.strip() for x in re.sub('"|\]|\[|[*]','',features).split(',')]
-    features[-1] = features[-1].replace(')','')
-    #return_code = "return-code_%s" %rest.replace('=','',1).strip()
-    new_line.op = command
-    new_line.args = features
-    new_line.retval = int(rest.replace('=','',1).strip())
-    new_line.out_syscall()
-    #print(f"feature {features} \n")
-    return new_line
 
 files = []
+deleted_files = []
 with open("trace.log",'r',encoding='utf8') as log_file:
     lines = log_file.readlines()
     for line in lines:
-        parse_command(line)
-
+        linesys = parse_strace(line)
+        linesys.do_syscall()
+#show_files(files)
+#show_files(deleted_files)
 print("done")
 
