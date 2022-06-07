b9637cf51fb198c9c6d9ad8f965bdf2e426a411b
yuanxu4 <yuanxu4@illinois.edu>
2022-06-06 22:31:50 -0500

    <feature> add a lot of feature of strace parser

--- strace_parser.py
+++ strace_parser.py
@@ -2,227 +2,21 @@ import re
 import os
 from re import compile as recompile
 import turtle
-
-class file_t:
-    def __init__(self):
-        self.filepath = ""
-        self.create_time = ""
-        self.delete_time = ""
-        self.fd_cur = []
-        self.size = 0
-        self.off = 0
-        self.OPflow = []
-    
-    filepath = ""
-    create_time = ""
-    delete_time = ""
-    fd_cur = []
-    size = -1
-    off = -1
-    OPflow = []
-
-    def out_file(self):
-        tplt = "{0:{3}^10}\t{1:{3}^10}\t{2:^10}"
-        print(f"{self.filepath : <50} {self.create_time:<15} - {self.delete_time:<15} size: {self.size:<20}")
-        #print(f"{self.create_time:<15} - {self.delete_time:<15} size: {self.size:<20}")
-
-    def out_OPflow(self):
-        for sys in self.OPflow:
-            print(f"{sys.time:<15} {sys.op:<10} {str(sys.args):<130} {sys.retval:<10}")
-
-
-
-class syscall:
-    time = ""
-    op = ""
-    args = []
-    retval = 0
-
-    def do_err(self, msg):
-        self.out_syscall()
-        print(msg)
-        exit(0)
-        
-    def out_syscall(self):
-        print(f"{self.time :<15} {self.op:<10} {self.args:<50} {self.retval:<10}")
-    
-    def do_syscall(self):
-        if self.op == "openat":
-            for file in files:
-                #open a exist file
-                if file.filepath == self.args[1]:
-                    if self.retval not in file.fd_cur:
-                            file.fd_cur.append(self.retval)
-                    file.OPflow.append(self)
-                    return 
-            # create a new file
-            new_file = file_t()
-            new_file.fd_cur = []
-            new_file.filepath = self.args[1]
-            new_file.fd_cur.append(self.retval)
-            if "O_CREAT" in self.args[-2]:
-                new_file.create_time = self.time
-            else:
-                #if the file create before we ignore its original size only keep the size change
-                new_file.create_time = "create before"
-            new_file.size = 0
-            new_file.off = 0
-            new_file.OPflow = []
-            new_file.OPflow.append(self)
-            files.append(new_file)
-            return 
-
-        elif self.op == "close":
-            for file in files:
-                if int(self.args[0]) in file.fd_cur:
-                    file.fd_cur.remove(int(self.args[0])) 
-                    file.OPflow.append(self)
-                    return 
-            for file in deleted_files:
-                if int(self.args[0]) in file.fd_cur:
-                    file.fd_cur.remove(int(self.args[0])) 
-                    file.OPflow.append(self)
-                    return 
-            #show_files(deleted_files)
-            self.do_err("cant close a file that not open")
-
-
-        elif self.op == "write":
-            if int(self.args[0]) == 1 or int(self.args[0]) == 2:
-                return 0
-            if int(self.args[-1]) != self.retval:
-                self.do_err("write request size and reture size is not match")
-            for file in files:
-                if int(self.args[0]) in file.fd_cur:
-                    if file.off == file.size:
-                        file.off += self.retval
-                        file.size += self.retval
-                    elif file.off != file.size:
-                        file.off += self.retval
-                        if file.off > file.size:
-                            file.size = file.off
-                    file.OPflow.append(self)
-                    return 
-            for file in deleted_files:
-                if int(self.args[0]) in file.fd_cur:
-                    if file.off == file.size:
-                        file.off += self.retval
-                        file.size += self.retval
-                    elif file.off != file.size:
-                        file.off += self.retval
-                        if file.off > file.size:
-                            file.size = file.off
-                    file.OPflow.append(self)
-                    return 
-            self.do_err("cant write into a file that not open")
-        
-        elif self.op == "lseek":
-            for file in files:
-                if int(self.args[0]) in file.fd_cur:
-                    if self.retval < 0:
-                        self.do_err("cant change the offset to the negative")            
-                    file.off = self.retval
-                    if file.off > file.size:
-                        file.size = file.off
-                    file.OPflow.append(self)
-                    return 
-            self.do_err("cant lseek change a file that not open")
-
-        elif self.op == "unlink":
-            for file in files:
-                if file.filepath == self.args[0]:
-                    file.delete_time = self.time
-                    file.OPflow.append(self)
-                    files.remove(file)
-                    deleted_files.append(file)
-            # if not find the file just ignore this op
-            return 
-        
-        # elif self.op == "fsync" | self.op == "fdatasync" | self.op == "pread64" | self.op == "fcntl" | self.op == "newfstatat" | self.op == "fstatfs":
-        #     for file in files:
-        #         if file.fd_cur == int(self.args[0]):
-        #             file.OPflow.append(self)
-        #             return 0
-        #     self.out_syscall()
-        #     print("cant fsync a file that not exist")
-        #     exit(0)
-        elif self.op == "fallocate":
-            for file in files:
-                if int(self.args[0]) in file.fd_cur:
-                    if int(self.args[3]) > file.size:
-                        file.size = int(self.args[3])
-                    file.OPflow.append(self)
-                    return 
-            for file in deleted_files:
-                if int(self.args[0]) in file.fd_cur:
-                    if int(self.args[3]) > file.size:
-                        file.size = int(self.args[3])
-                    file.OPflow.append(self)
-                    return 
-            self.do_err("cant fallocate a file that not open")
-
-        elif self.op == "rename":
-            for file in files:
-                if file.filepath == self.args[0]:
-                    file.filepath = self.args[1]
-                    file.OPflow.append(self)
-                    return 
-
-            self.do_err("can't rename a file not exist")
-
-        elif self.op == "mkdir":
-            new_file = file_t()
-            new_file.fd_cur = []
-            new_file.OPflow = []
-            new_file.filepath = self.args[0]
-            new_file.create_time = self.time
-            new_file.OPflow.append(self)
-            files.append(new_file)
-            return 
-        
-        elif self.op == "rmdir":
-            for file in files:
-                if file.filepath == self.args[0]:
-                    file.delete_time = self.time
-                    file.OPflow.append(self)
-                    files.remove(file)
-                    deleted_files.append(file)
-            # if not find the file just ignore this op
-            return 
-
-
-        elif self.op == "fsync" or self.op == "fdatasync":
-            for file in files:
-                if int(self.args[0]) in file.fd_cur:
-                    file.OPflow.append(self)
-                    return 
-            self.do_err("cant sync the file not open")
-
-        elif self.op == "pread64" or self.op == "fcntl":
-            for file in files:
-                if int(self.args[0]) in file.fd_cur:
-                    file.OPflow.append(self)
-                    return 
-            self.do_err("cant sync the file not open")
-
-        elif self.op == "read":
-            for file in files:
-                if int(self.args[0]) in file.fd_cur:
-                    file.OPflow.append(self)
-                    file.off += self.retval
-import re
-import os
-from re import compile as recompile
-import turtle
 import sys
 import numpy as np
 import math
 from datetime import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
+import datetime
 
 LEVEL_0 = 0
 LEVEL_1 = 1
 LEVEL_2 = 2
 
+pp = PdfPages('BW.pdf')
 class file_t:
     def __init__(self):
         self.filepath = ""
@@ -275,8 +69,26 @@ class file_t:
         read_bw /= granularity
         write_bw /= granularity
         print(f"READ BW: {read_bw}")
-        print(f"WRITE BW: {write_bw}")
 
+        t = mdates.drange(workload_start,workload_end,datetime.timedelta(seconds=0.1))
+        # plt.xlabel('T/0.1 sec')
+        # plt.ylabel('Bandwith/ Byte/sec')
+        # plt.title('read bw')
+        # plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S.%f'))
+        # plt.plot(t,read_bw)
+        # plt.gcf().autofmt_xdate()
+        # plt.savefig(f'{self.filepath}Writebw.pdf')
+        print(f"WRITE BW: {write_bw}")
+        fig = plt.figure(figsize=(20, 4))
+        plt.xlabel("T/0.1 sec")
+        plt.ylabel("Bandwith/ Byte/sec")
+        pt = self.filepath.replace("/tmp/rocksdbtest-1003/dbbench/", "")
+        plt.title(f"{pt}:{self.create_time}")
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S.%f'))
+        plt.plot(t, write_bw)
+        plt.gcf().autofmt_xdate()
+        pp.savefig(fig)
+        plt.close(fig)
 
 class syscall:
     time = ""
@@ -552,3 +364,70 @@ if __name__ == "__main__":
     print("===================DELETED FILES============================")
     show_files(deleted_files)
 
+    fnamesc = []
+    createtimes = []
+    fnamesd = []
+    deletetimes = []
+    #add create time into list
+    for file in files:
+        if file_importance(file) == LEVEL_2:
+            fnamesc.append(file.filepath.replace("/tmp/rocksdbtest-1003/dbbench/", ""))
+            createtimes.append(file.create_time)
+    for file in deleted_files:
+        if file_importance(file) == LEVEL_2:
+            fnamesc.append(file.filepath.replace("/tmp/rocksdbtest-1003/dbbench/", ""))
+            createtimes.append(file.create_time)
+    #add delete file into list 
+    for file in deleted_files:
+        if file_importance(file) == LEVEL_2:
+            fnamesd.append(file.filepath.replace("/tmp/rocksdbtest-1003/dbbench/", ""))
+            deletetimes.append(file.delete_time)
+
+    
+
+    # Convert strings (e.g. 18:30:29.293920) to datetime
+    createtimes = [dt.strptime(d, "%H:%M:%S.%f") for d in createtimes]
+    deletetimes = [dt.strptime(d, "%H:%M:%S.%f") for d in deletetimes]
+
+    # Choose some nice levels
+    createlevels = np.tile([12,11,10,9,8, 7,6,5,4, 3,2, 1],
+                    int(np.ceil(len(createtimes)/12)))[:len(createtimes)]
+
+    deletelevels = np.tile([-12,-11,-10,-9,-8, -7,-6,-5,-4, -3,-2, -1],
+                    int(np.ceil(len(deletetimes)/12)))[:len(deletetimes)]
+
+    # Create figure and plot a stem plot with the date
+    fig, ax = plt.subplots(figsize=(60, 4), constrained_layout=False)
+    ax.set(title="file time line")
+
+    ax.vlines(createtimes, 0, createlevels, color="tab:red")  # The vertical stems.
+    ax.vlines(deletetimes, 0, deletelevels, color="tab:blue")  # The vertical stems.
+    ax.plot(createtimes, np.zeros_like(createtimes), "-o",
+            color="k", markerfacecolor="w")  # Baseline and markers on it.
+    ax.plot(deletetimes, np.zeros_like(deletetimes), "-o",
+            color="k", markerfacecolor="w")  # Baseline and markers on it.
+
+    # annotate lines
+    for d, l, r in zip(createtimes, createlevels, fnamesc):
+        ax.annotate(r, xy=(d, l),
+                    xytext=(-3, np.sign(l)*3), textcoords="offset points",
+                    horizontalalignment="right",
+                    verticalalignment="bottom" if l > 0 else "top")
+    for d, l, r in zip(deletetimes, deletelevels, fnamesd):
+        ax.annotate(r, xy=(d, l),
+                    xytext=(-3, np.sign(l)*3), textcoords="offset points",
+                    horizontalalignment="right",
+                    verticalalignment="bottom" if l > 0 else "top")
+
+    # format xaxis with 4 month intervals
+    # ax.xaxis.set_major_locator(mdates.MonthLocator(interval=4))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M:%S"))
+    plt.setp(ax.get_xticklabels(), rotation=30, ha="right")
+
+    # remove y axis and spines
+    ax.yaxis.set_visible(False)
+    ax.spines[["left", "top", "right"]].set_visible(False)
+
+    ax.margins(y=0.1)
+    plt.savefig("timeline.pdf")
+    pp.close()
