0a531c890f051699f5bbd0449c94d637ef4a82b2
yuanxu4 <yuanxu4@illinois.edu>
2022-06-07 01:19:36 -0500

    <create> merge_numpy.py is for merging all syscall into a numpy file

--- merge_numpy.py
+++ merge_numpy.py
@@ -1,44 +1,41 @@
 
-import os
-import string
-import turtle
 import sys
+import os
 import numpy as np
-import math
-
-def parse_trace(line:string):
-    args = line.split()
-    newsys = syscall()
-    newsys.seq = int(args[0])
-    newsys.time = args[1]
-    newsys.op = args[2]
-    newsys.inode = int(args[3])
-    newsys.isize = int(args[4])
-    if len(args) > 6:
-        newsys.off = int(args[5])
-        newsys.opsize = int(args[6])
-    return newsys
+import re
 
 if __name__ == "__main__":
     # set input file name
-    TRACE_FILE = "test"
+    TRACE_DIR = "../gsf-filesrv/"
     if len(sys.argv) >= 2:
         TRACE_FILE = sys.argv[1]
-    
-    line_threshold = 1000000#float('inf')
+    line_threshold = 100#float('inf')
 
+    openfiles = [f for f in os.listdir(TRACE_DIR)]
 
-    with open(TRACE_FILE,'r',encoding='utf8') as log_file:
-        lines = log_file.readlines()
-        for i, line in enumerate(lines):
-            linesys = parse_trace(line)
-            linesys.do_syscall()
-            if i >= line_threshold:
+    lines = []
+    print("Starting adding lines...")
+    for i, f in enumerate(openfiles):
+        with open(TRACE_DIR + f,'r',encoding='utf8') as log_file:
+            lines += log_file.readlines()
+            log_file.close()
+        print(f"done file {i} " + f)
+    print(f"The lines length is {len(lines)}")
+
+    rep = {"OPEN": "0", "CLOSE": "1", "READ": "2", "WRITE": "3","FSYNC":"4", "FDATASYNC":"5"," HIT":""," MISS":""} 
+    rep = dict((re.escape(k), v) for k, v in rep.items()) 
+    pattern = re.compile("|".join(rep.keys()))
+
+    print("Start adding the line into narrary")
+    all_trace = np.zeros((min(len(lines),line_threshold),7))
+    print(f"create a numpy array with shape:{all_trace.shape} type:{all_trace.dtype}")
+    for i,l in enumerate(lines):
+        l = pattern.sub(lambda m: rep[re.escape(m.group(0))], l)
+        args = l.split()
+        args = (args + 7 * ["0"])[:7]
+        all_trace[i] = args
+        if i >= line_threshold - 1:
                 break
-            if i != 0 and i % 100000 == 0:
-                print(f"{i} lines parsed")
-    for f in files.values():
-        print("=========================================================================================")
-        f.print_file()
-        f.print_overlap()
-        print("=========================================================================================")
+        if i != 0 and i % 1000000 == 0:
+            print(f"{i} lines added")
+
---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+a = np.arange(10).reshape(2,5)
+
+print(a)
+print (a[1,2])
+
+
+b = [0,0,0]
+b = (b + 7 * ["0"])[:7]
